a235c4eb5e8d1c6b9980594c144b3fcbf26d65ad
iainbrux <me@iainbrux.com>
2021-06-30 23:51:09 +0100

    9.3 started

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,29 @@
-const calculateBMI = (weightInKg: number, heightInMeters: number) => {
-  console.log(Math.floor(weightInKg / (heightInMeters ** 2)))
+interface Measurements {
+  weight: number,
+  height: number
 }
 
-calculateBMI(106.2, 1.88)
+const parseNodeArguments = (args: Array<string>): Measurements => {
+  if (args.length < 4) throw new Error('Not enough arguments were passed');
+  if (args.length > 4) throw new Error('Too many arguments were passed');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      weight: Number(args[2]),
+      height: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers.');
+  }
+}
+
+const calculateBMI = (weightInKg: number, heightInMeters: number) => {
+  console.log(`Your BMI is ${Math.floor(weightInKg / (heightInMeters ** 2))}.`);
+}
+
+try {
+  const { weight, height } = parseNodeArguments(process.argv);
+  calculateBMI(weight, height)
+} catch (err) {
+  console.log('An error occured. Error message:', err.message)
+}
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,3 +1,34 @@
-const calculateExercises = () => {
-  
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
 }
+
+const calculateExercises = (hours: Array<number>): Result => {
+  const periodLength: number = hours.length;
+  const trainingDays: number = hours.filter(day => day !== 0).length;
+  const target: number = 4;
+  const success: boolean = trainingDays > target ? true : false
+  const rating: number = Number((trainingDays / periodLength * 100).toFixed(2));
+  const average: number = Number((hours.reduce((acc, val) => acc + val, 0) / hours.length).toFixed(2));
+  const ratingDescription: string = trainingDays > target ? 'Amazing!' : trainingDays === target ? 'Ok' : trainingDays < target && trainingDays > 0 ? 'Needs improvement' : 'Lazy'
+  const result: Result = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    target,
+    average,
+    ratingDescription
+  }
+
+  console.log(result)
+
+  return result
+}
+
+calculateExercises([3, 0, 2, 4.5, 0, 3, 1])
