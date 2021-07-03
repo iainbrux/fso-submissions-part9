1abe47909fdfea59b65c1ddc03a103bde85040f5
Iain Brookes <iain.brookes@outlook.com>
2021-07-04 00:32:59 +0100

    to continue on pc

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,13 +8,31 @@ interface Result {
   average: number
 }
 
-const calculateExercises = (hours: Array<number>): Result => {
+interface WorkoutInput {
+  target: number,
+  workouts: Array<number>
+}
+
+const parseArguments = (args: Array<string>): WorkoutInput => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const target = Number(args[2]);
+  args.shift();
+  const workouts = args.map(arg => Number(arg));
+
+  return {
+    target,
+    workouts
+  }
+}
+
+const calculateExercises = (target: number, hours: Array<number>): Result => {
   const periodLength: number = hours.length;
   const trainingDays: number = hours.filter(day => day !== 0).length;
-  const target: number = 4;
   const success: boolean = trainingDays > target ? true : false
   const rating: number = Number((trainingDays / periodLength * 100).toFixed(2));
   const average: number = Number((hours.reduce((acc, val) => acc + val, 0) / hours.length).toFixed(2));
+  // const average: number = 12;
   const ratingDescription: string = trainingDays > target ? 'Amazing!' : trainingDays === target ? 'Ok' : trainingDays < target && trainingDays > 0 ? 'Needs improvement' : 'Lazy'
   const result: Result = {
     periodLength,
@@ -26,9 +44,15 @@ const calculateExercises = (hours: Array<number>): Result => {
     ratingDescription
   }
 
+  console.log(average)
   console.log(result)
 
   return result
 }
 
-calculateExercises([3, 0, 2, 4.5, 0, 3, 1])
+try {
+  const {target, workouts} = parseArguments(process.argv);
+  calculateExercises(target, workouts);
+} catch (err) {
+  console.log('An error has occured. Error message:', err.message)
+}
