e0178466e352f5f32b07fd2f43d7ad49fdf9b4e8
iainbrux <me@iainbrux.com>
2021-07-06 10:06:02 +0100

    small refactor for readability

--- index.ts
+++ index.ts
@@ -14,40 +14,25 @@ app.get('/bmi', (req, res) => {
   if(req.query.weight && req.query.height) {
     try {
       const { weightInKg, heightInCm, bmi } = calculateBMI(Number(req.query.weight), Number(req.query.height));
-      res.status(200).send({
-        weightInKg,
-        heightInCm,
-        bmi
-      }).json();
+      res.status(200).send({ weightInKg, heightInCm, bmi }).json();
     } catch(err) {
-      if (err instanceof Error) {
-        res.status(400).send({
-          error: err.message
-        }).json();
-      }
+      if (err instanceof Error) res.status(400).send({ error: err.message }).json();
     }
   } else {
-    res.status(400).send({
-      error: 'malformatted perameters'
-    }).json();
+    res.status(400).send({ error: 'malformatted perameters' }).json();
   }
-  res.status(400).send({
-    error: 'malformatted perameters'
-  }).json();
 });
 
 app.post('/exercises', (req, res) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   if (req.body.target && req.body.workouts) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { target, workouts }: WorkoutInput = req.body;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (typeof req.body.target !== 'number' || !Array.isArray(req.body.workouts)) {
       res.status(400).send({ error: "malformatted perameters" }).json().end();
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { target, workouts }: WorkoutInput = req.body;
-
     workouts.forEach(num => typeof num !== 'number' ? res.status(400).send({ error: "malformatted perameters" }).end() : false);
-
     const responseBody = calculateExercises(target, workouts);
     res.status(201).send(responseBody).json().end();
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
